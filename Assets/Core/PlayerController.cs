72c7185c196e7c16bdc8d025e2e040f4b366a4b0
Mikhail S. Kataev <lpestlname@gmail.com>
2019-07-02 08:57:21 +0300

    Added some structures

---
+++ Assets/Core/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerControllerState { BEFORE_GAME = 0, DICE = 1, CHOICE_PAWN = 2, WAIT = 3, AFTER_GAME = 4 };
+
+public enum Team { GREEN = 0, BLUE = 1, YELLOW = 2, RED = 3 };
+
+[System.Serializable]
+public struct Player
+{
+    public PlayerController Controller;
+    public Team Team;
+}
+
+public class PlayerController : MonoBehaviour
+{
+    private PlayerControllerState _state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
